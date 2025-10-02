c6dae9331bc53fdd6325563abf15dc9bd66bcdb1
Artorias- <artorias0107@gmail.com>
2025-10-03 04:03:19 +0530

    Update CORS settings for Netlify deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = 3000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: 'https://artorias-2.netlify.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
