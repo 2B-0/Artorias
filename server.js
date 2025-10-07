e49abd84715d29610855a0d7b7e2a27178112804
Artorias- <artorias0107@gmail.com>
2025-10-08 03:28:43 +0530

    Something changed

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'frontend')));
+app.use(express.static(path.join(__dirname,'../frontend')));
 
 // Replace your session middleware with:
 app.use(session({
